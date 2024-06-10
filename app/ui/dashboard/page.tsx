1e1aaf58993ca381c1dc1218e0aa0d3420d0400f
jason090687 <jason.piit21@gmail.com>
2024-06-10 16:27:45 +0800

    commit

--- app/ui/dashboard/page.tsx
+++ app/ui/dashboard/page.tsx
@@ -7,13 +7,13 @@ import { Card } from './cards';
  
 export default async function Page() {
     const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
+    const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue() to finish
     const {
         numberOfInvoices,
         numberOfCustomers,
         totalPaidInvoices,
         totalPendingInvoices,
-      } = await fetchCardData();
+    } = await fetchCardData(); // wait for fetchLatestInvoices() to finish
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
